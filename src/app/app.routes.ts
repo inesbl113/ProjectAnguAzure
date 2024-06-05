30d67cb564a838b4b1186aa7655adf4d6f712740
inesbl113 <inesbl113@gmail.com>
2024-06-05 14:22:50 +0200

    Update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './component/home/home.component';
+import { InscriptionComponent } from './component/inscription/inscription.component';
+import { ConnexionComponent } from './component/connexion/connexion.component';
+import { TwittesComponent } from './component/twittes/twittes.component';
+import { CurrentComponent } from './component/current/current.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{ path: '', redirectTo: 'home', pathMatch: 'full' },
+	{ path: 'home', component: HomeComponent },
+	{ path: 'inscription', component: InscriptionComponent },
+	{ path: 'connexion', component: ConnexionComponent },
+	{ path: 'tweet', component: TwittesComponent },
+	{ path: 'popo/:id', component: CurrentComponent }
+];
